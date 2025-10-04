519f122301289310085be31d2d7cfea4cd3dce98
thesavant42 <jbrashars@gmail.com>
2025-10-03 18:31:34 -0700

    Refactor comments in feels.py to clarify emotional expressions and model details

--- feels.py
+++ feels.py
@@ -5,6 +5,19 @@ from transformers import pipeline, AutoTokenizer, logging
 # Set transformers logging to show only errors
 logging.set_verbosity_error()
 
+# Trained on 28 different emotional expressions
+#
+# https://huggingface.co/joeddav/distilbert-base-uncased-go-emotions-student/tree/main
+#
+# | admiration | curiosity | fear | optimism |
+# | amusement | desire | gratitude | pride |
+# | anger | disappointment | grief | realization |
+# | annoyance | disapproval | joy | relief |
+# | approval | disgust | love | remorse |
+# | caring | embarrassment | nervousness | sadness |
+# | confusion | excitement | neutral | surprise |
+#
+
 model_id = "joeddav/distilbert-base-uncased-go-emotions-student"
 
 # print(f"Loading standard model: {model_id}")
